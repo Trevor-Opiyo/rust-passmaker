370c5d73b7783a766e9c0eba77719437f4c26366
Trevor Opiyo <46691585+Trevor-Opiyo@users.noreply.github.com>
2021-08-12 15:03:30 -0500

    Added ability to regenerate password

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use rand::seq::SliceRandom;
 
 fn main() {
     let mut options = Vec::new();
+    let mut password_length: usize;
     let mut lower_case = vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     let mut upper_case = vec!['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    let mut integers = vec!['0','1','3','4','5','6','7','8','9'];
+    let mut integers = vec!['0','1','2','3','4','5','6','7','8','9'];
     let mut special_characters = vec!['!','@','#','$','^','&','*','(',')','_','-','+','=','<','>','?'];
 
     header();
@@ -23,15 +24,7 @@ fn main() {
         options.append(&mut special_characters)
     }
 
-    let password_length = get_length();
-    let password: Vec<_> = options
-        .choose_multiple(&mut rand::thread_rng(), password_length)
-        .collect();
-
-    print!("\n");
-    for index in password {
-        print!("{}", index);
-    }
+    generate_password(options, password_length)
 }
 
 fn header() {
@@ -66,4 +59,28 @@ fn get_length()-> usize {
     io::stdin().read_line(&mut answer).ok();
     let answer: usize = answer.trim().parse().unwrap();
     return answer;
+}
+
+fn generate_password<T>(password_options: Vec<T>, password_length: usize) {
+    loop {
+        let password: Vec<_> = password_options;
+        password.choose_multiple(&mut rand::thread_rng(), password_length).collect();
+        print!("\n");
+        for index in password {
+            print!("{}", index);
+        }
+        println!("\nWould you like to regenerate the password with the same parameters? [Y/N]?\n");
+        let mut answer = String::new();
+        io::stdin().read_line(&mut answer).ok();
+        let answer: String = answer.trim().parse().unwrap();
+        match answer.as_str() {
+            "Y" | "y" | "YES" | "Yes" | "yes" => {}
+            "N" | "n" | "NO" | "No" | "no" => {
+                break;
+            }
+            _=> {
+                println!("\nPlease enter valid input. ex: \'Y\' or \'N\'");
+            }
+        }
+    }
 }
